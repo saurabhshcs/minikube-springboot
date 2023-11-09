a61cbe9e9a800776129932234b47aa47ba312fdf
saurabhshcs <saurabhshcs@gmail.com>
2023-11-09 00:44:43 +0000

    Project structure

---
+++ src/main/java/com/saurabhshcs/minikubespringboot/MinikubeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.minikubespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinikubeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinikubeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhshcs/minikubespringboot/MinikubeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.minikubespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinikubeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
